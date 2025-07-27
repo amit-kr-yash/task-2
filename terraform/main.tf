a24d0021cd5671335a7e392106e55bdae1847ec5
amit-kr-yash <codewithyash10@gmail.com>
2025-07-27 20:58:23 +0530

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "my-insecure-devsecops-bucket-12345"
+  acl    = "public-read" # This will be flagged by tfsec
+}
+
+# A more secure version for comparison
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-secure-devsecops-bucket-12345"
+  acl    = "private"
+
+  # Block all public access
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+}
